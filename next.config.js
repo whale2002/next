da968444d2cde13a83cbcab4add53c3a173c8eee
chin <1277180540@qq.com>
2022-10-27 12:01:23 +0800

    :sparkles: add backend

--- next.config.js
+++ next.config.js
@@ -10,6 +10,16 @@ const nextConfig = {
       '@': path.resolve(__dirname)
     }
     return config
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '1337',
+        pathname: '/uploads/**'
+      }
+    ]
   }
 }
 
